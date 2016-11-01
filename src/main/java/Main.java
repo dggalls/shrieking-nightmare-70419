bdf9041ce163f92008e2f53e0e1a59c258560ccc
dggalls <danny.ogallchoir3@mail.dcu.ie>
2016-11-01 17:15:03 +0000

    Small changes to main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,11 +20,11 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
+    get("/hello", (req, res) -> "Hello Danny");
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
+            attributes.put("message", "Hello Danny");
 
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
